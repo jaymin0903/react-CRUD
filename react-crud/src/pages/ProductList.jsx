141b90358e36cb29dbbb57e3534b1787d0dd4b4a
jaymin0903 <148968840+jaymin0903@users.noreply.github.com>
2024-03-20 17:19:08 +0530

    added new features add, edit and delete

--- react-crud/src/pages/ProductList.jsx
+++ react-crud/src/pages/ProductList.jsx
@@ -2,8 +2,15 @@ import { FilterMatchMode, FilterOperator } from "primereact/api";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { Dialog } from "primereact/dialog";
+import { InputNumber } from "primereact/inputnumber";
 import { InputText } from "primereact/inputtext";
-import React, { useEffect, useState } from "react";
+import { InputTextarea } from "primereact/inputtextarea";
+import { RadioButton } from "primereact/radiobutton";
+import { Toast } from "primereact/toast";
+import { Toolbar } from "primereact/toolbar";
+import { classNames } from "primereact/utils";
+import React, { useEffect, useRef, useState } from "react";
 import { ProductService } from "../services/ProductService";
 
 function ProductDatatable() {
@@ -11,6 +18,24 @@ function ProductDatatable() {
   const [filters, setFilters] = useState(null);
   const [globalFilterValue, setGlobalFilterValue] = useState("");
   const [loading, setLoading] = useState(false);
+
+  let emptyProduct = {
+    id: null,
+    title: "",
+    description: "",
+    category: null,
+    price: 0,
+    stock: 0,
+  };
+
+  const [productDialog, setProductDialog] = useState(false);
+  const [deleteProductDialog, setDeleteProductDialog] = useState(false);
+  const [deleteProductsDialog, setDeleteProductsDialog] = useState(false);
+  const [product, setProduct] = useState(emptyProduct);
+  const [selectedProducts, setSelectedProducts] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const toast = useRef(null);
+  const dt = useRef(null);
 
   useEffect(() => {
     ProductService.getProductsMedium().then((data) => {
@@ -58,11 +83,190 @@ function ProductDatatable() {
 
   const header = renderHeader();
 
+  const openNew = () => {
+    setProduct(emptyProduct);
+    setSubmitted(false);
+    setProductDialog(true);
+  };
+
+  const hideDialog = () => {
+    setSubmitted(false);
+    setProductDialog(false);
+  };
+
+  const hideDeleteProductDialog = () => {
+    setDeleteProductDialog(false);
+  };
+
+  const hideDeleteProductsDialog = () => {
+    setDeleteProductsDialog(false);
+  };
+
+  const saveProduct = () => {
+    setSubmitted(true);
+
+    if (product.title.trim()) {
+      let _products = [...products];
+      let _product = { ...product };
+
+      if (product.id) {
+        const index = findIndexById(product.id);
+
+        _products[index] = _product;
+        toast.current.show({ severity: "success", summary: "Successful", detail: "Product Updated", life: 3000 });
+      } else {
+        _product.id = createId();
+        _products.push(_product);
+        toast.current.show({ severity: "success", summary: "Successful", detail: "Product Created", life: 3000 });
+      }
+
+      setProducts(_products);
+      setProductDialog(false);
+      setProduct(emptyProduct);
+    }
+  };
+
+  const editProduct = (product) => {
+    setProduct({ ...product });
+    setProductDialog(true);
+  };
+
+  const confirmDeleteProduct = (product) => {
+    setProduct(product);
+    setDeleteProductDialog(true);
+  };
+
+  const deleteProduct = () => {
+    let _products = products.filter((val) => val.id !== product.id);
+
+    setProducts(_products);
+    setDeleteProductDialog(false);
+    setProduct(emptyProduct);
+    toast.current.show({ severity: "success", summary: "Successful", detail: "Product Deleted", life: 3000 });
+  };
+
+  const findIndexById = (id) => {
+    let index = -1;
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  };
+
+  const createId = () => {
+    let id = "";
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return id;
+  };
+
+  const exportCSV = () => {
+    dt.current.exportCSV();
+  };
+
+  const confirmDeleteSelected = () => {
+    setDeleteProductsDialog(true);
+  };
+
+  const deleteSelectedProducts = () => {
+    let _products = products.filter((val) => !selectedProducts.includes(val));
+
+    setProducts(_products);
+    setDeleteProductsDialog(false);
+    setSelectedProducts(null);
+    toast.current.show({ severity: "success", summary: "Successful", detail: "Products Deleted", life: 3000 });
+  };
+
+  const onCategoryChange = (e) => {
+    let _product = { ...product };
+
+    _product["category"] = e.value;
+    setProduct(_product);
+  };
+
+  const onInputChange = (e, title) => {
+    const val = (e.target && e.target.value) || "";
+    let _product = { ...product };
+
+    _product[`${title}`] = val;
+
+    setProduct(_product);
+  };
+
+  const onInputNumberChange = (e, title) => {
+    const val = e.value || 0;
+    let _product = { ...product };
+
+    _product[`${title}`] = val;
+
+    setProduct(_product);
+  };
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <>
+        <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={() => editProduct(rowData)} />
+        <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => confirmDeleteProduct(rowData)} />
+      </>
+    );
+  };
+
+  const productDialogFooter = (
+    <>
+      <Button label="Cancel" icon="pi pi-times" outlined onClick={hideDialog} />
+      <Button label="Save" icon="pi pi-check" onClick={saveProduct} />
+    </>
+  );
+  const deleteProductDialogFooter = (
+    <>
+      <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteProductDialog} />
+      <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteProduct} />
+    </>
+  );
+  const deleteProductsDialogFooter = (
+    <>
+      <Button label="No" icon="pi pi-times" outlined onClick={hideDeleteProductsDialog} />
+      <Button label="Yes" icon="pi pi-check" severity="danger" onClick={deleteSelectedProducts} />
+    </>
+  );
+  const leftToolbarTemplate = () => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        <Button label="New" icon="pi pi-plus" severity="success" onClick={openNew} />
+        <Button
+          label="Delete"
+          icon="pi pi-trash"
+          severity="danger"
+          onClick={confirmDeleteSelected}
+          disabled={!selectedProducts || !selectedProducts.length}
+        />
+      </div>
+    );
+  };
+
+  const rightToolbarTemplate = () => {
+    return <Button label="Export" icon="pi pi-upload" className="p-button-help" onClick={exportCSV} />;
+  };
+
   return (
     <>
+      <Toast ref={toast} />
       <h2 style={{ textAlign: "center" }}>Products List</h2>
       <div className="card">
+        <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
         <DataTable
+          ref={dt}
+          selection={selectedProducts}
+          onSelectionChange={(e) => setSelectedProducts(e.value)}
           value={products}
           paginator
           sortMode="multiple"
@@ -75,15 +279,175 @@ function ProductDatatable() {
           rows={5}
           rowsPerPageOptions={[5, 10, 20, 50]}
           tableStyle={{ minWidth: "50rem" }}
-          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrePageLink CurrentPageReport NextPageLink LastPageLink"
-          currentPageReportTemplate="{first} to {last} of {totalRecords}">
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrePageLink pageLinks CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{first} to {last} of {totalRecords} products">
+          <Column selectionMode="multiple" exportable={false}></Column>
           <Column field="id" sortable header="Sr. No." style={{ width: "10%" }}></Column>
           <Column field="title" sortable header="Title" filter filterPlaceholder="Search by title" style={{ width: "35%" }}></Column>
           <Column field="brand" sortable header="Brand" filter filterPlaceholder="Search by brand" style={{ width: "15%" }}></Column>
           <Column field="price" sortable header="Price" style={{ width: "10%" }}></Column>
           <Column field="category" sortable header="Category" filter filterPlaceholder="Search by category" style={{ width: "15%" }}></Column>
           <Column field="stock" header="Stock" style={{ width: "10%" }}></Column>
+          <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: "12rem" }}></Column>
         </DataTable>
+
+        <Dialog
+          visible={productDialog}
+          style={{ width: "32rem" }}
+          breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+          header="Product Details"
+          modal
+          className="p-fluid"
+          footer={productDialogFooter}
+          onHide={hideDialog}>
+          <div className="field">
+            <label htmlFor="title" className="font-bold">
+              title
+            </label>
+            <InputText
+              id="title"
+              value={product.title}
+              onChange={(e) => onInputChange(e, "title")}
+              required
+              autoFocus
+              className={classNames({ "p-invalid": submitted && !product.title })}
+            />
+            {submitted && !product.title && <small className="p-error">title is required.</small>}
+          </div>
+          <div className="field">
+            <label htmlFor="description" className="font-bold">
+              Description
+            </label>
+            <InputTextarea
+              id="description"
+              value={product.description}
+              onChange={(e) => onInputChange(e, "description")}
+              required
+              rows={3}
+              cols={20}
+            />
+          </div>
+
+          <div className="field">
+            <label className="mb-3 font-bold">Category</label>
+            <div className="formgrid grid">
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category1"
+                  name="category"
+                  value="smartphones"
+                  onChange={onCategoryChange}
+                  checked={product.category === "smartphones"}
+                />
+                <label htmlFor="category1">Smartphones</label>
+              </div>
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category2"
+                  name="category"
+                  value="laptops"
+                  onChange={onCategoryChange}
+                  checked={product.category === "laptops"}
+                />
+                <label htmlFor="category2">Laptops</label>
+              </div>
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category3"
+                  name="category"
+                  value="fragrances"
+                  onChange={onCategoryChange}
+                  checked={product.category === "fragrances"}
+                />
+                <label htmlFor="category3">Fragrances</label>
+              </div>
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category4"
+                  name="category"
+                  value="skincare"
+                  onChange={onCategoryChange}
+                  checked={product.category === "skincare"}
+                />
+                <label htmlFor="category4">Skincare</label>
+              </div>
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category4"
+                  name="category"
+                  value="groceries"
+                  onChange={onCategoryChange}
+                  checked={product.category === "groceries"}
+                />
+                <label htmlFor="category4">Groceries</label>
+              </div>
+              <div className="field-radiobutton col-6">
+                <RadioButton
+                  inputId="category4"
+                  name="category"
+                  value="home-decoration"
+                  onChange={onCategoryChange}
+                  checked={product.category === "home-decoration"}
+                />
+                <label htmlFor="category4">Home-Decoration</label>
+              </div>
+            </div>
+          </div>
+
+          <div className="formgrid grid">
+            <div className="field col">
+              <label htmlFor="price" className="font-bold">
+                Price
+              </label>
+              <InputNumber
+                id="price"
+                value={product.price}
+                onValueChange={(e) => onInputNumberChange(e, "price")}
+                mode="currency"
+                currency="USD"
+                locale="en-US"
+              />
+            </div>
+            <div className="field col">
+              <label htmlFor="stock" className="font-bold">
+                Stock
+              </label>
+              <InputNumber id="stock" value={product.stock} onValueChange={(e) => onInputNumberChange(e, "stock")} />
+            </div>
+          </div>
+        </Dialog>
+
+        <Dialog
+          visible={deleteProductDialog}
+          style={{ width: "32rem" }}
+          breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+          header="Confirm"
+          modal
+          footer={deleteProductDialogFooter}
+          onHide={hideDeleteProductDialog}>
+          <div className="confirmation-content">
+            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: "2rem" }} />
+            {product && (
+              <span>
+                Are you sure you want to delete <b>{product.title}</b>?
+              </span>
+            )}
+          </div>
+        </Dialog>
+
+        <Dialog
+          visible={deleteProductsDialog}
+          style={{ width: "32rem" }}
+          breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+          header="Confirm"
+          modal
+          footer={deleteProductsDialogFooter}
+          onHide={hideDeleteProductsDialog}>
+          <div className="confirmation-content">
+            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: "2rem" }} />
+            {product && <span>Are you sure you want to delete the selected products?</span>}
+          </div>
+        </Dialog>
       </div>
     </>
   );
